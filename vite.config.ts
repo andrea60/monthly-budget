144c4ff219effb84aa37e090a09b1af233e0bba8
Andrea60 <a.algeri.1996@gmail.com>
2025-01-23 18:25:07 +0000

    Change MonzoClient

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,25 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    TanStackRouterVite(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "envs/prod/settings.json",
-          dest: "public",
-        },
-      ],
-    }),
-  ],
+export default defineConfig(({ command }) => {
+  const isProdBuild = command === "build";
+
+  let plugins = [react(), TanStackRouterVite()];
+
+  if (isProdBuild)
+    plugins = [
+      ...plugins,
+      viteStaticCopy({
+        targets: [
+          {
+            src: "envs/prod/settings.json",
+            dest: "public",
+          },
+        ],
+      }),
+    ];
+
+  return {
+    plugins,
+  };
 });
