d6a8a7f3439beae55b2188f102daf5df2c0921f3
Andrea60 <a.algeri.1996@gmail.com>
2025-01-23 11:15:15 +0000

    Add plugin to copy static asset files

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/settings.json",
+          dest: "public",
+        },
+      ],
+    }),
+  ],
 });
