637e18bf77bf000b7c8efdacf2e9f3359e5ff862
Andrea60 <a.algeri.1996@gmail.com>
2025-01-24 18:04:52 +0000

    Implement secure client secret function

---
+++ functions/clientSecret.ts
@@ -0,0 +1,43 @@
+type Env = Record<string, string>;
+
+export const onRequest: PagesFunction<Env> = async (context) => {
+  const authHeader = context.request.headers.get("Authorization");
+  if (!authHeader)
+    return new Response("No Authorization header found", { status: 401 });
+
+  const [authSchema, authValue] = parseAuthHeader(authHeader);
+  if (authSchema !== "Basic")
+    return new Response("Authentication method not supported", { status: 400 });
+
+  const [username, password] = parseNameAndPassword(authValue);
+
+  const passcodeKey = "passcode_" + username;
+  if (!(passcodeKey in context.env))
+    return new Response("User not known", { status: 403 });
+
+  const passcode = context.env[passcodeKey];
+
+  if (passcode !== password)
+    return new Response("Invalid password", { status: 403 });
+
+  const secretKey = "client_secret_" + username;
+  const clientSecret = context.env[secretKey] || "";
+
+  const responsebBody = { clientSecret };
+  return new Response(JSON.stringify(responsebBody));
+};
+
+const parseAuthHeader = (header: string): [string, string] => {
+  const parts = header.split(" ");
+  if (parts.length !== 2)
+    throw new Error("Invalid Authorization header detected");
+
+  return parts as [string, string];
+};
+
+const parseNameAndPassword = (authHeaderValue: string): [string, string] => {
+  const parts = atob(authHeaderValue).split(":");
+  if (parts.length !== 2) throw new Error("Invalid Authorization header value");
+
+  return parts as [string, string];
+};
